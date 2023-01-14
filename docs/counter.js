e031852e33fb80fbd28f4103eb4c27f3f9931d42
Harukka <47594192+Harukka@users.noreply.github.com>
2023-01-14 10:22:08 +0900

    counter

--- docs/counter.js
+++ docs/counter.js
@@ -1,14 +1,13 @@
 
 function count(){
     //現在の値を取得
-//    var thisCount = $("#count").html();
-    var thisCount = document.getElementById('count');
-
+    var thisCount = $("#count").html();
+    
     //文字列から数値に変換
         thisCount = Number(thisCount);
         //取得した数値に＋１
         thisCount = thisCount +1;
     //計算した値を上書き
-    //$("#count").html(thisCount);
-    document.getElementById('count').textContent=thisCount;
+    $("#count").html(thisCount);
+ 
 }
